35ace68937b6e10d7eeaf188bc44e17edb335553
keitosuwahara <210103gi@yse-c.net>
2022-10-10 18:35:14 +0900

    linebotのyoutube みよう

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
 j_file=open("./static/json/info.json","r")
 info=json.load(j_file)
-print(info["CHANNEL_ACCESS_TOKEN"])
+
+CHANNEL_ACCESS_TOKEN=info["CHANNEL_ACCESS_TOKEN"]
+line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    
