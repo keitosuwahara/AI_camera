a7abbeae7c5fe577ff26a74ea8e75d216e32841f
keitosuwahara <210103gi@yse-c.net>
2022-10-10 17:41:45 +0900

    line-bot-sdkインストール

--- ling_test3.py
+++ ling_test3.py
@@ -2,5 +2,5 @@ import MeCab
 import unidic
 #やっと辞書を入れれた
 tagger = MeCab.Tagger('-Owakati -d "C:/Program Files/MeCab/dic/ipadic" -u "C:/Program Files (x86)/MeCab/mecabfolder/userdic/mecab-user-dict-seed.dic"')
-result = tagger.parse('私が最近見た映画は、約束のネバーランドでした。')
+result = tagger.parse('私が最近見た映画は、約束のネバーランドでした、あと鬼滅の刃、呪術廻戦')
 print(result)
