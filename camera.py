06ea887e1e806a353a2870bdc87aa8096966086a
keitosuwahara <108104699+keitosuwahara@users.noreply.github.com>
2022-11-02 13:16:43 +0900

    up

--- camera.py
+++ camera.py
@@ -2,14 +2,15 @@ import cv2
 
 def camera():
     camera = cv2.VideoCapture(0)
-
+    alpha ="abcdefghijkmnlopqrstuwxyz"
+    keypress = input()
     while True:
         ret, frame = camera.read() #フレーム取得
         cv2.imshow("camera", frame) #フレームを画面に表示
-
         #qを押すとwhileループを抜ける
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+        
 
     #撮影用オブジェクトとウィンドウの解放
     camera.release()
