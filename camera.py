9ad8c15ed53cfe900fc600f6647041747a6db0c0
keitosuwahara <108104699+keitosuwahara@users.noreply.github.com>
2022-11-02 10:27:41 +0900

    Added camera

---
+++ camera.py
@@ -0,0 +1,17 @@
+import cv2
+
+def camera():
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = camera.read() #フレーム取得
+        cv2.imshow("camera", frame) #フレームを画面に表示
+
+        #qを押すとwhileループを抜ける
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    #撮影用オブジェクトとウィンドウの解放
+    camera.release()
+    cv2.destroyAllWindows()
+
