285cae140197ed6adaeb742f500f9c08b8a84e4d
keitosuwahara <108104699+keitosuwahara@users.noreply.github.com>
2022-11-30 14:36:55 +0900

    jsでtxt読み込み方法を調べる

--- static/js/displaydb.js
+++ static/js/displaydb.js
@@ -1,19 +1,38 @@
-let fs = new ActiveXObject("Scripting.FileSystemObject");
-let file = fs.OpenTextFile("../../database/NumOfPeapledb.txt");
-/* 1行目のみ読み込む */
-text[0] = file.ReadLine();
-console.log(text[0])
+file = new FileReader();//Filereaderインスタンス作成
+
+file.readAsText("../../database/NumOfPeapledb.txt");//txtファイル読み込み
+
+console.log(file.result);
 
 
-let table = document.getElementById("targetTable");//htmlの表に接続
-let newRow = table.insertRow();
-let newCell = newRow.insertCell();
-let newText = document.createTextNode(' ');
-newCell.appendChild(newText);
 
-newCell = newRow.insertCell();
-newText = document.createTextNode(18);
-newCell.appendChild(newText);
 
-//テキストファイルの中の数字を行数にする
+
+
+
+
+
+
+
+
+
+let rows = ["aa","aaaa","aaaa","aaaa","aa","a","sd","dd"];//行に入る情報//ひとまずダミー情報を入れておく
+let table = document.getElementById("getTable");//table内の情報取得
+
+let colCount = table.rows[0].cells.length;//列数
+let rowCount = rows.length;//行数
+
+console.log("列数は："+colCount+"行数は："+rowCount);
+
+
+
+
+
+
+
+
+
+
+
+
 
