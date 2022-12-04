735b0065115974a739324d7f2276512e837ebe75
keitosuwahara <210103gi@yse-c.net>
2022-12-04 09:35:28 +0900

    operatedb.py 途中

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from camera import camera
+from createdb import studentdb, admindb
+from printxl import printxl
+from flask import Flask, render_template, request
+import sqlite3
+import datetime
 
-camera()
+# DBを作成する（既に作成されていたらこのDBに接続する
+dbname ="./database/students.db"
+conn = sqlite3.connect(dbname)
+#SQLiteを操作するためのカーソル,コントローラー
+cur = conn.cursor()
+#データベース内の表示
+info = []
+records = cur.execute("SELECT * FROM students")
+for record in records:
+    info.append(list(record))
+#print(info)
+# コミットしないと登録が反映されない
+
+conn.commit()
+#print(info)#[[186758, '志村燿平', 1], [190721, '岩橋大地', 1], [200284, '桃崎奏斗', 1], [210103, '諏訪原慶斗', 1], [435755, '高田悠', 0], [557855, '黒野怜奈', 1], [846556, 'トゴーフーバダムツェレン', 1]]
+#print(len(info))#データベースの人数を出力
+
+key = ["学籍番号", "名前", "出席", "遅刻", "早退"]
+stdlist = [dict(zip(key,item)) for item in info]#keyとinfoをまとめて辞書型にする
+today = datetime.date.today()#今日の日付
+
+app = Flask(__name__)
+#トップページ
+@app.route("/", methods=["GET","POST"])
+def top():
+    return render_template("/displaydb.html", stdlist = stdlist, today = today)#stdlistはdb内の情報が辞書型で入っている変数
 
 
+
+studentdb()
+admindb()
+printxl()
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
--- printxl.py
+++ printxl.py
@@ -2,71 +2,67 @@ import openpyxl as xl
 import sqlite3
 import datetime
 
-from openpyxl.styles.borders import Border, Side
-from openpyxl.styles.alignment import Alignment
-from openpyxl.styles import PatternFill
-
 #db内の情報をエクエルに転記するプログラム
+def printxl():
+    dbname ="./database/students.db"
+    conn = sqlite3.connect(dbname)
+    #SQLiteを操作するためのカーソル,コントローラー
+    cur = conn.cursor()
+    #データベース内の表示
+    info = []
+    records = cur.execute("SELECT * FROM students")
+    for record in records:
+        info.append(list(record))
 
-dbname ="./database/students.db"
-conn = sqlite3.connect(dbname)
-#SQLiteを操作するためのカーソル,コントローラー
-cur = conn.cursor()
-#データベース内の表示
-info = []
-records = cur.execute("SELECT * FROM students")
-for record in records:
-    info.append(list(record))
+    conn.commit()
 
-conn.commit()
-
-key = ["学籍番号", "名前", "出席", "遅刻", "早退"]
-stdlist = [dict(zip(key,item)) for item in info]#keyとinfoをまとめて辞書型にする
-
-
-wb = xl.Workbook()
-ws = wb.worksheets[0]
-
-today = datetime.date.today()#今日の日付
-
-ws.cell(row = 1, column = 1, value = str(today)+"時点")
-
-for i in range(0,5):#キーのみを転記する
-    ws.cell(row = 1, column = i+2, value = key[i])
-
-#それぞれの属性の空リスト
-ids = []
-names = []
-attends = []
-tikokus = []
-soutais = []
-
-#それぞれの属性をリスト化
-for std in stdlist:
-    ids.append(std["学籍番号"])
-    names.append(std["名前"])
-    attends.append(std["出席"])
-    tikokus.append(std["遅刻"])
-    soutais.append(std["早退"])
-
-#学籍番号の転記
-for index, id in enumerate(ids):
-    ws.cell(row = index + 2, column = 2, value = id)
-#名前の転記
-for index, name in enumerate(names):
-    ws.cell(row = index + 2, column = 3, value = name)
-#出席の転記
-for index, attend in enumerate(attends):
-    ws.cell(row = index + 2, column = 4, value = attend)
-#遅刻の転記
-for index, tikoku in enumerate(tikokus):
-    ws.cell(row = index + 2, column = 5, value = tikoku)
-#早退の転記
-for index, soutai in enumerate(soutais):
-    ws.cell(row = index + 2, column = 6, value = soutai)
+    key = ["学籍番号", "名前", "出席", "遅刻", "早退"]
+    stdlist = [dict(zip(key,item)) for item in info]#keyとinfoをまとめて辞書型にする
 
 
-wb.save('./database/class1.xlsx')
+    wb = xl.Workbook()
+    ws = wb.worksheets[0]
+
+    today = datetime.date.today()#今日の日付
+
+    ws.cell(row = 1, column = 1, value = str(today)+"時点")
+
+    for i in range(0,5):#キーのみを転記する
+        ws.cell(row = 1, column = i+2, value = key[i])
+
+    #それぞれの属性の空リスト
+    ids = []
+    names = []
+    attends = []
+    tikokus = []
+    soutais = []
+
+    #それぞれの属性をリスト化
+    for std in stdlist:
+        ids.append(std["学籍番号"])
+        names.append(std["名前"])
+        attends.append(std["出席"])
+        tikokus.append(std["遅刻"])
+        soutais.append(std["早退"])
+
+    #学籍番号の転記
+    for index, id in enumerate(ids):
+        ws.cell(row = index + 2, column = 2, value = id)
+    #名前の転記
+    for index, name in enumerate(names):
+        ws.cell(row = index + 2, column = 3, value = name)
+    #出席の転記
+    for index, attend in enumerate(attends):
+        ws.cell(row = index + 2, column = 4, value = attend)
+    #遅刻の転記
+    for index, tikoku in enumerate(tikokus):
+        ws.cell(row = index + 2, column = 5, value = tikoku)
+    #早退の転記
+    for index, soutai in enumerate(soutais):
+        ws.cell(row = index + 2, column = 6, value = soutai)
+
+
+    wb.save('./database/class1.xlsx')
 
 
 
